f93a600e564a43e309eb0fa6c60fae6d1af5b0b0
zataara <bryantmac@gmail.com>
2021-12-13 23:00:49 -0800

    added NavBar and Switch Routes to App

--- src/App.js
+++ src/App.js
@@ -1,58 +1,77 @@
+import { Switch, Routes, Route, Redirect } from "react-router-dom";
+import NavBar from "./NavBar";
+import DogList from "./DogList";
+import DogDetails from "./DogDetails";
 
+import "./App.css";
 
-import './App.css';
+import whiskey from "./images/whiskey.jpg";
+import tubby from "./images/tubby.jpg";
+import duke from "./images/duke.jpg";
+import perry from "./images/perry.jpg";
 
-function App() {
+function App({ dogs }) {
   return (
     <div>
-
+      <NavBar />
+      <Switch>
+        <Routes>
+          <Route exact path="/dogs">
+            <DogList dogs={dogs} />
+          </Route>
+          <Route path="/dogs/:name">
+            <DogDetails dogs={dogs} />
+          </Route>
+          <Redirect to="/dogs" />
+        </Routes>
+      </Switch>
     </div>
   );
 }
 
-App.defaultProps = {
-  dogs: [
-    {
-      name: "Whiskey",
-      age: 5,
-      src: './whiskey.jpg',
-      facts: [
-        "Whiskey loves eating popcorn.",
-        "Whiskey is a terrible guard dog.",
-        "Whiskey wants to cuddle with you!"
-      ]
-    },
-    {
-      name: "Duke",
-      age: 3,
-      src: './duke.jpg',
-      facts: [
-        "Duke believes that ball is life.",
-        "Duke likes snow.",
-        "Duke enjoys pawing other dogs."
-      ]
-    },
-    {
-      name: "Perry",
-      age: 4,
-      src: '/.perry.jpg',
-      facts: [
-        "Perry loves all humans.",
-        "Perry demolishes all snacks.",
-        "Perry hates the rain."
-      ]
-    },
-    {
-      name: "Tubby",
-      age: 4,
-      src: './tubby.jpg',
-      facts: [
-        "Tubby is really stupid.",
-        "Tubby does not like walks.",
-        "Angelina used to hate Tubby, but claims not to anymore."
-      ]
-    }
-  ]
-}
+export const dogs = [
+  {
+    name: "Whiskey",
+    age: 5,
+    src: whiskey,
+    facts: [
+      "Whiskey loves eating popcorn.",
+      "Whiskey is a terrible guard dog.",
+      "Whiskey wants to cuddle with you!"
+    ]
+  },
+  {
+    name: "Duke",
+    age: 3,
+    src: duke,
+    facts: [
+      "Duke believes that ball is life.",
+      "Duke likes snow.",
+      "Duke enjoys pawing other dogs."
+    ]
+  },
+  {
+    name: "Perry",
+    age: 4,
+    src: perry,
+    facts: [
+      "Perry loves all humans.",
+      "Perry demolishes all snacks.",
+      "Perry hates the rain."
+    ]
+  },
+  {
+    name: "Tubby",
+    age: 4,
+    src: tubby,
+    facts: [
+      "Tubby is really stupid.",
+      "Tubby does not like walks.",
+      "Angelina used to hate Tubby, but claims not to anymore."
+    ]
+  }
+];
+
+App.defaultProps = { dogs };
 
 export default App;
